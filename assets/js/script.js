a156f9a6acefe6b939d3928fd1c7d0ee3dec9d62
Lyes Kryos <82159357+LyesKryos@users.noreply.github.com>
2026-04-03 10:45:43 -0400

    Remove updateNavTop function from script.js
    Removed updateNavTop function that adjusts sidenav position.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,6 @@
 const header = document.querySelector('header');
 const sidenav = document.querySelector('.sidenav');
 
-function updateNavTop() {
-  sidenav.style.top = header.offsetHeight + 'px';
-}
-
 window.addEventListener('load', updateNavTop);
 window.addEventListener('resize', updateNavTop);
 
